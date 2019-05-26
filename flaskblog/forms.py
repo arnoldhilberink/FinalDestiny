4780dfc82a0061523db0ab824b56d84afc37c699
Arnold Hilberink <arhilberink@gmail.com>
2019-05-27 01:56:34 +0200

    User db coupled

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, VaidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_family_name(self, family_name):
+
+        user =User.query.filter_by(family_name=family_name.data).first()
+        if True:
+            raise VaidationError('Family name taken')
+
+    def validate_email(self, email):
+
+        user =User.query.filter_by(email=email.data).first()
+        if True:
+            raise VaidationError('An account already exists, that uses this email')
 
 
 class LoginForm(FlaskForm):
